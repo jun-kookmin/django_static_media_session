8e033113eafdbc32755901819515ef185c362cf4
Song Jimin <songwlals050328@gmail.com>
2024-04-05 14:25:21 +0900

    과제제출

--- config/blog/models.py
+++ config/blog/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+import os
+from django.conf import settings
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='blog/', null=True)
+
+    def __str__(self):
+        return self.title
+
+    def delete(self, *args, **kargs):
+        if self.image:
+            os.remove(os.path.join(settings.MEDIA_ROOT, self.image.path))
+        super(Blog, self).delete(*args, **kargs)
--- config/blog/views.py
+++ config/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog
+from django.core.paginator import Paginator
 
 # READ
 def home(request):
     blogs = Blog.objects.all()
-    return render(request, 'home.html', {'blogs': blogs})
+    paginator = Paginator(blogs, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home.html', {'page_obj': page_obj})
 
 # DETAIL READ
 def detail(request, blog_id):
@@ -21,6 +25,7 @@ def create(request):
     new_blog = Blog()
     new_blog.title = request.POST['title']
     new_blog.content = request.POST['content']
+    new_blog.image = request.FILES.get('image')
     new_blog.save()
     return redirect('detail', new_blog.id)
     # return render(request, 'detail.html', {'blog': new_blog})
